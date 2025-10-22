f70609a763ccf7d44e668eaeb2fb20914cc8b009
Ben Burtenshaw <ben.burtenshaw@gmail.com>
2025-10-22 17:20:41 +0000

    use docker image of atari env

--- examples/atari_simple.py
+++ examples/atari_simple.py
@@ -24,7 +24,7 @@ def main():
     """Run a simple Atari episode."""
     # Connect to the Atari environment server
     print("Connecting to Atari environment...")
-    env = AtariEnv(base_url="http://localhost:8000")
+    env = AtariEnv.from_docker_image("ghcr.io/meta-pytorch/openenv-atari-env:latest")
 
     try:
         # Reset the environment

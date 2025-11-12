3f1cd047c03f033bd11c0c6a848bdea777154973
Yuan He <yuanhe.cs.ai@gmail.com>
2025-11-12 00:38:47 +0000

    chore: update doc

--- src/envs/websearch_env/server/__init__.py
+++ src/envs/websearch_env/server/__init__.py
@@ -4,9 +4,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""Searchr1 Env environment server components."""
+"""WebSearch Env environment server components."""
 
-from .searchr1_env_environment import Searchr1Environment
+from .websearch_env_environment import WebSearchEnvironment
 
-__all__ = ["Searchr1Environment"]
+__all__ = ["WebSearchEnvironment"]
 

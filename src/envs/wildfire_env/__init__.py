0281e212fb0c9cbe5618f31e62c2bfb92a0672f6
shankerram3 <shankerram3@users.noreply.github.com>
2026-01-04 14:53:56 -0700

    Remove unnecessary markdown and PowerShell files to match upstream patterns

--- src/envs/wildfire_env/__init__.py
+++
@@ -1,9 +0,0 @@
-from .models import WildfireAction, WildfireObservation, WildfireState
-from .client import WildfireEnv
-
-__all__ = [
-    "WildfireAction",
-    "WildfireObservation",
-    "WildfireState",
-    "WildfireEnv",
-]
